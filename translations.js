33163ad5dc0944f9f058fdbf8c7b8057908e309f
Nguyen Van Thien <thiennguyen0710.dn@gmail.com>
2026-08-19 16:04:04 +0700

    Cập nhật bản dịch cho các sản phẩm da và dây đeo đồng hồ

--- translations.js
+++ translations.js
@@ -16,7 +16,24 @@ const exactTranslationMap = {
   'Sully goatskin leather wallet Light Cream': 'Da dê vân Sully Màu Light Cream',
   'Epsom calfskin leather wallet Gold': 'Da bê vân Epsom Màu Nâu vàng (Gold)',
   'Lagun cowhide leather wallet Ficelle': 'Da bò vân Lagun Màu Trắng Đục (Ficelle)',
-  'Sully goatskin leather wallet Noisette': 'Da dê vân Sully Màu Noisette'
+  'Sully goatskin leather wallet Noisette': 'Da dê vân Sully Màu Noisette',
+  'Epsom calfskin leather watch straps HAAS Cream': 'Da bê vân Epsom HAAS Màu Kem (Cream)',
+  'Epsom calfskin leather watch straps Chocolate': 'Da bê vân Epsom Màu Chocolate',
+  'Sully goatskin leather watch straps Acier': 'Da dê vân Sully Màu Acier',
+  'Patnat goatskin leather watch straps Patchouli': 'Da dê vân Patnat màu Patechouli',
+  'Lagun cowhide leather watch straps Orange': 'Da bò vân Lagun Màu Cam (Orange)',
+  'Epsom calfskin leather watch straps Noir black': 'Da bê vân Epsom Màu Đen (Noir)',
+  'Togo Odessa Weinheimer calfskin leather watch straps black': 'Da bê Odessa vân Togo weinheimer màu đen black',
+  'Sully goatskin leather watch straps Bleu Vista': 'Da dê vân Sully Màu Bleu Vista',
+  'Epsom calfskin leather wallet Etoupe': 'Da bê vân Epsom Màu Ghi Nâu (Etoupe)',
+  'Sully goatskin leather wallet Myrrhe': 'Da dê vân Sully Màu Myrrhe',
+  'Sully grain goatskin leather wallet Myrrhe': 'Da dê vân Sully Màu Myrrhe',
+  'Sully grain goatskin leather wallet Colibri': 'Da dê vân Sully Màu Colibri',
+  'Sully goatskin leather wallet Colibri': 'Da dê vân Sully Màu Colibri',
+  'Epsom calfskin leather wallet HAAS Cream': 'Da bê vân Epsom HAAS Màu Kem (Cream)',
+  'Epsom calfskin leather wallet Chocolate': 'Da bê vân Epsom Màu Chocolate',
+  'Sully goatskin leather wallet Oursin': 'Da dê vân Sully Màu Oursin',
+  'Sully grain goatskin leather wallet Oursin': 'Da dê vân Sully Màu Oursin'
 };
 
 function normalizeText(text) {
